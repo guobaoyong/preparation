a495e13da6083f7032e53f0252c2ee1f0ecf67e3
zsh <43240825@qq.com>
2019-10-28 21:20:15 +0800

    加入扫描定时任务的注解

--- src/main/java/qqzsh/top/preparation/PreparationApplication.java
+++ src/main/java/qqzsh/top/preparation/PreparationApplication.java
@@ -2,8 +2,10 @@ package qqzsh.top.preparation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PreparationApplication {
 
     public static void main(String[] args) {
